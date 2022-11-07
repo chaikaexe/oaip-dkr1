e19d6b3535314194e659e92842b8043df95c5771
chaikaexe <49320557+chaikaexe@users.noreply.github.com>
2022-11-07 10:51:24 +0300

    Add files via upload

---
+++ dom.pas
@@ -0,0 +1,15 @@
+﻿program dom1;
+var x,y:real;
+begin
+  readln(x);
+  while (x>=-11) and (x<=0) do begin
+  if x<-9 then 
+    y:=sin(x)*10-tan(x);
+  if (x>=-9) and (x<-2) then 
+    y:=-x/power(x,1/3)+power(x,2);
+  if x>=-2 then
+    y:=cos(x)*cos(2*x)-power(x,0.1*x);
+  writeln('x=',x,'; y=',y:3:2);
+  x:=x+0.3;
+  end;
+end.
